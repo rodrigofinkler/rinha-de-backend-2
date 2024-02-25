627c642b8bc81da6703bbd49f594f1304068384e
Rodrigo Finkler <finklerrg@gmail.com>
2024-02-24 21:50:09 -0300

    First commit with initial TS express server

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+export default app;
