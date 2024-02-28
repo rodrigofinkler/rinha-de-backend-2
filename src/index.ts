227e508525ac5fa4bd188b5413691fb4f212f419
Rodrigo Finkler <finklerrg@gmail.com>
2024-02-27 22:39:24 -0300

    Add config to make the server capable of handling a JSON body + Add the draft of the main requests that the system must handle

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Router, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,9 +6,28 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 6969;
 
-app.get('/', (_req: Request, res: Response) => {
+const routes = Router();
+
+// GET routes
+routes.get('/', (_req: Request, res: Response) => {
   res.send('😎');
 });
+
+routes.get('/clientes/:id/extrato', (req: Request, res: Response) => {
+  const { id } = req.params;
+  res.json({ 'id': id });
+});
+
+// POST routes
+routes.post('/clientes/:id/transacoes', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { valor, tipo, descricao } = req.body;
+  console.log(req.body);
+  res.json({ id, valor, tipo, descricao });
+});
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
