0b80ed064771bb46019556372bb9c68853aaf4b2
Rodrigo Finkler <finklerrg@gmail.com>
2024-05-16 21:03:12 -0300

    Comment out insertions that were used in development

--- database/init/setup.sql
+++ database/init/setup.sql
@@ -29,15 +29,15 @@ INSERT INTO cliente (saldo, limite) VALUES (0,500000);
 
 -- NEXT LINES WERE USED FOR DEBUGGING SETUP
 
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 100, 'd', 'TR1', NOW());
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 200, 'd', 'TR2', NOW());
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 300, 'd', 'TR3', NOW());
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 400, 'd', 'TR4', NOW());
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 500, 'd', 'TR5', NOW());
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 600, 'd', 'TR6', NOW());
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 700, 'd', 'TR7', NOW());
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 800, 'd', 'TR8', NOW());
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 900, 'd', 'TR9', NOW());
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 1000, 'd', 'TR10', NOW());
-INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 1100, 'd', 'TR11', NOW());
-UPDATE cliente SET saldo = 93400 WHERE id = 1;
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 100, 'd', 'TR1', NOW());
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 200, 'd', 'TR2', NOW());
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 300, 'd', 'TR3', NOW());
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 400, 'd', 'TR4', NOW());
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 500, 'd', 'TR5', NOW());
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 600, 'd', 'TR6', NOW());
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 700, 'd', 'TR7', NOW());
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 800, 'd', 'TR8', NOW());
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 900, 'd', 'TR9', NOW());
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 1000, 'd', 'TR10', NOW());
+-- INSERT INTO transacao (id_cliente, valor, tipo, descricao, realizada_em) VALUES ( 1, 1100, 'd', 'TR11', NOW());
+-- UPDATE cliente SET saldo = 93400 WHERE id = 1;
